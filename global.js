6fd87280d6750c7e4176a5d2253a7f95594a65b0
Eric Caleb <calebme@mail.com>
2016-09-03 09:05:54 +0100

    Process argv array and flags

--- global.js
+++ global.js
@@ -3,5 +3,20 @@ var path = require("path")
 
 global.console.log(`iamcaleberic ${path.basename(__filename)}`)
 
-console.log(process.argv) 
+console.log(process.argv); // argument variables used to start process
 
+// Get argv array flags
+function grab(flag) {
+	// body...
+	var index =  process.argv.indexOf(flag);
+	return (index === -1 ) ? null : process.argv[index+1] 
+}
+
+var greeting =  grab('--greeting');
+var user = grab('--user');
+
+if (!user || !greeting){
+	console.log("You blew it");
+} else {
+	console.log(`welcome ${user} , ${greeting}`);
+}
