88a188ee1ca7c3353e87366bf4b636df2621f5f1
Eric Caleb <calebme@mail.com>
2016-09-04 09:50:33 +0100

    imported v8 and outputed heapstatistics

--- core.js
+++ core.js
@@ -1,6 +1,7 @@
 // require core modules 
 var path =  require('path'); 
-var util = require('util'); 
+var util =  require('util'); 
+var v8   =  require('v8');
 
 // pluck base file
 util.log(path.basename(__filename));
@@ -8,3 +9,5 @@ util.log(path.basename(__filename));
 var dirUploads = path.join(__dirname , 'www' , 'files' , 'uploads');
 
 util.log(dirUploads); // util module similar to console.log but adds time to output
+
+util.log(v8.getHeapStatistics());
