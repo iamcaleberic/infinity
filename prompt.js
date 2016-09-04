2c769751b3cdac934a8b24f79ba1009af8a9ef0d
Eric Caleb <calebme@mail.com>
2016-09-04 10:05:37 +0100

    Using readline methods and litseners

--- prompt.js
+++ prompt.js
@@ -1,8 +1,21 @@
 var readline =  require('readline');
-
 var rl = readline.createInterface(process.stdin , process.stdout);
 
+var realNigga = {
+	name: '',
+	sayings: []
+};
+
 rl.question("What robot disguised person do you know? " , function(answer) {
+	realNigga.name = answer;
+
+	rl.setPrompt(`What would ${realNigga.name} say?`);
+	
+	rl.prompt();
+
+	rl.on('line', function(saying) {
+		console.log(saying.trim());
+	})
 
 	console.log(answer);
-})
+});
