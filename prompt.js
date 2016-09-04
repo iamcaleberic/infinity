a19ff154a8077aac743e6dd2c00179f67fc984f1
Eric Caleb <calebme@mail.com>
2016-09-04 10:21:32 +0100

    Added exit litsener

--- prompt.js
+++ prompt.js
@@ -7,6 +7,7 @@ var realNigga = {
 };
 
 rl.question("What robot disguised person do you know? " , function(answer) {
+	
 	realNigga.name = answer;
 
 	rl.setPrompt(`What would ${realNigga.name} say?`);
@@ -14,11 +15,23 @@ rl.question("What robot disguised person do you know? " , function(answer) {
 	rl.prompt();
 
 	rl.on('line', function(saying) {
+		realNigga.sayings.push(saying.trim());
 
-		rl.setPrompt(`What else would ${realNigga.name} say? ('exit' to leave)`);
+		if (saying.toLowerCase().trim() === 'exit'){
+			rl.close();
+		} else{
+			rl.setPrompt(`What else would ${realNigga.name} say? ('exit' to leave)`);
 
-		rl.prompt();
+			rl.prompt();
+		}
+		
 	})
 
-	console.log(answer);
+
 });
+
+rl.on('close' , function(){
+
+	console.log("%s is a real person that says %j", realNigga.name , realNigga.sayings);
+	process.exit();
+})
