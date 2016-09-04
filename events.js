f0779cb9dcf23b87833c2ac607e377edffe87537
Eric Caleb <calebme@mail.com>
2016-09-04 10:47:36 +0100

    Event Emitter custom function

--- events.js
+++ events.js
@@ -1,10 +1,44 @@
-var events = require('events');
+var EventEmitter = require('events').EventEmitter;
+var util = require('util');
 
-var emitter =  new events.EventEmitter();
+var Nigga = function(name){
+	this.name = name;
+}  
 
-emitter.on('defunct' , function(message, status) {
 
-	console.log(`${status}: ${message}`);
-})
+util.inherits(Nigga , EventEmitter);
 
-emitter.emit('defunct' , "England The United Kingdom" , 500);
+var cesar = new Nigga("Jasourne Bourne");
+
+cesar.on('speak' , function(said){
+
+	console.log(`${this.name}: ${said} `);
+
+});
+
+cesar.emit('speak' , "Welcome to culiacan Mexico");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var emitter =  new events.EventEmitter();
+
+// emitter.on('defunct' , function(message, status) {
+
+// 	console.log(`${status}: ${message}`);
+// })
+
+// emitter.emit('defunct' , "England The United Kingdom" , 500);
