f87b68f81cb131cfb054b16ef84541f052b6231b
Eric Caleb <calebme@mail.com>
2016-09-04 16:23:18 +0100

    Added Looping and stdout function

---
+++ parle.js
@@ -0,0 +1,21 @@
+var parle = [
+	
+	"I feel chemicals burning in my bloodstream",
+	"Bicken back being bool",
+	"Like a boss",
+	"Silence is sometimes the most powerful weapon"
+
+
+];
+
+var interval =  setInterval(function() {
+
+	var i =  Math.floor(Math.random() * parle.length);
+	process.stdout.write(`${parle[i]} \n`);
+} , 1000);
+
+process.stdin.on('data' , function(data) {
+
+	console.log(`STDIN Data Received -> ${data.toString().trim()}`);
+
+});
