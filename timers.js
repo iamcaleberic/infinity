6c4560c05b4513c1fe2c8f95d9c7eafd40900d98
Eric Caleb <calebme@mail.com>
2016-09-04 09:20:13 +0100

    Clear interval functions for a timer

--- timers.js
+++ timers.js
@@ -4,14 +4,17 @@ var waitInterval = 500;
 
 console.log("Boom");
 
-setInterval(function(){
+var interval = setInterval(function(){
+	
 	currentTime += waitInterval;
 
 	console.log(`waiting ${currentTime/1000} seconds...`);
-})
+},waitInterval)
 
 setTimeout(function(){
 
+
+	clearInterval(interval);
 	console.log("Finito");
 
 }, waitTime)
