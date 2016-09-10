dc6a1d9f2a6de122a58d8d058b73d4c7bc3afb7f
Eric Caleb <calebme@mail.com>
2016-09-10 14:22:47 +0100

    Added Comments to remove function

--- remove.js
+++ remove.js
@@ -1,6 +1,15 @@
 var fs =  require("fs");
+// sync function with try block for error catching
 try{
     fs.unlinkSync("./lib/Europe.md");
 } catch(err){
     console.log(err);
 }
+// async
+fs.unlink("./lib/city.md" , function(err){
+    if(err){
+        console.log(err);
+    }else {
+        console.log("Poof! Removed")
+    }
+})
