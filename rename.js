312660cb48aa4af32d3ecb89eeb055e90a8d4483
Eric Caleb <calebme@mail.com>
2016-09-10 14:12:26 +0100

    Added fs functions for renaming /moving files

---
+++ rename.js
@@ -0,0 +1,14 @@
+var fs =  require("fs");
+// sync rename
+fs.renameSync("./lib/city.md", "./lib/europe.md");
+
+console.log("renamed");
+// Async rename
+fs.rename("./lib/europe.md", "./Europe.md" , function(err){
+    if (err){
+        console.log(err);
+    }else{
+        console.log("Moved successfully");
+    }
+
+});
