60fd22d76d8226e2dc1166152ed56adb967085ed
Eric Caleb <calebme@mail.com>
2016-09-10 14:36:35 +0100

    Modified directories.js to add fs remove directory function

--- directories.js
+++ directories.js
@@ -1,5 +1,23 @@
 var fs =  require("fs");
+try{
+    fs.renameSync("./assets/logs", "./logs");
+    console.log("Directory moved");
+} catch(err){
+    console.log(err);
+};
 
-fs.renameSync("./assets/logs", "./logs");
 
-console.log("Directory moved");
+fs.rmdir("assets",function(err){
+    if (err){
+        throw err;
+    }
+
+    console.log("Directory Removed");
+});
+
+fs.rmdir("./logs", function(err){
+    if (err){
+        throw err;
+    }
+    console.log("Poof! Gone");
+})
