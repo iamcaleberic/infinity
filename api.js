405648d2c88d77c33e076ee461e54f58ef558ae3
Eric Caleb <calebme@mail.com>
2016-09-12 17:43:03 +0100

    Stringify json data

--- api.js
+++ api.js
@@ -2,5 +2,11 @@ var http =  require("http");
 var data = require("./data/country");
 
 http.createServer(function(req, res){
-    
-}).litsen(4000)
+
+    res.writeHead(200 , {"Content-Type":"text/json"});
+
+    res.end(JSON.stringify(data));
+
+}).listen(4000);
+
+console.log("Server litsening on port 4000");
