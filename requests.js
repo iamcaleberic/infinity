a841fbfdb33b1c898f29ecbbaa939b98f8caf6a5
Eric Caleb <calebme@mail.com>
2016-09-12 15:30:10 +0100

    Add litsener for headers output

--- requests.js
+++ requests.js
@@ -7,3 +7,21 @@ var options = {
     path:"/",
     method: "GET"
 }
+
+var req = http.request(options , function(res){
+    var responseBody = "";
+    console.log("Response from server started");
+    console.log(`Server Status: ${res.statusCode}`);
+    console.log("Response Headers: %j", res.headers);
+
+    res.set("UTF-8");
+
+    res.once("data", function(chunk){
+        console.log(chunk)
+    });
+
+    res.on("data" , function(chunk){
+        console.log(`--chunk--${chunk.length}`);
+        responseBody += chunk;
+    })
+});
