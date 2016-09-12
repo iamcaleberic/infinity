0bbe59c566d1527dd75802aee7cdc4cfe76d606f
Eric Caleb <calebme@mail.com>
2016-09-12 16:32:43 +0100

    Added createserver function for file server

--- fileserver.js
+++ fileserver.js
@@ -1,3 +1,21 @@
 var http = require("http");
 var fs  = require("fs");
 var path = require("path");
+
+http.createServer(function(req, res){
+    console.log(`${req.method} request for ${req.url}`);
+    
+    if (req.url === "/"){
+        fs.readFile("./public/index.html", "UTF-8", function(err, html){
+        res.writeHead(200 ,{"Content-Type":"text/html"} );
+        res.html;
+
+        });
+
+    } else {
+        res.writeHead(404 , {"Content-Type":"text/plain"});
+        res.end("404 Ghost File");
+    }
+
+
+}).litsen(4000);
