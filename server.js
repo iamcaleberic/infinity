4532ff3194f8e1bd694117d1529426be9da23201
Eric Caleb <calebme@mail.com>
2016-09-12 16:06:27 +0100

    Modified content type def

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 var http = require("http");
 var server = http.createServer(function(req , res){
 
-    res.writeHead(200,{"Content-Type: text/plain"});
+    res.writeHead(200, {"Content-Type": "text/plain"});
 
     res.end("Hello World");
 
 });
 
 server.litsen(4000);
-console.log("server is running on port 4000"); 
+console.log("server is running on port 4000");  
