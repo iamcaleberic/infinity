82e84fb42f51b5a100decb585e3e556f3e4bcf1d
Eric Caleb <calebme@mail.com>
2016-09-18 11:25:24 +0100

    Added filestream and pipe response to browser

--- formserver.js
+++ formserver.js
@@ -1,2 +1,9 @@
 var http = require("http");
 var fs = require("fs");
+
+http.createServer(function(req, res){
+    res.writeHead(200, {"Content-Type":"text/html"});
+    fs.createReadStream("./public/form.html" , "UTF-8").pipe(res);
+}).listen(5000);
+
+console.log("Form server litsening on port 5000");
